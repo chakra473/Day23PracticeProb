e94415e27332acbb3fe2c58ccdc371d425c485a6
chakravarthy <m.chakra473@gmail.com>
2022-04-10 13:49:37 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day23practiceProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to AddressBook problem");
+
+            Records records = new Records();
+
+
+
+            //Infinite Loop
+            while (true)
+            {
+                Console.WriteLine("\nEnter 1 to Add person in AddressBook");
+                Console.WriteLine("Enter 2 to Display");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        records.AddRecord();
+                        break;
+                    case 2:
+                        records.ToPrint();
+                        break;
+                    default:
+                        Console.WriteLine("Pleasee select valid input");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
